80baebcf6f490fe41b8eee73b93d5666b88905ad
saramortara <saramortara@gmail.com>
2024-01-07 10:30:05 -0300

    adding maps for 2022 and 2023

--- scripts/mapa.R
+++ scripts/mapa.R
@@ -2,30 +2,25 @@
 library(ggplot2)
 library(dplyr)
 library(sf)
-library(rnaturalearth)
-library(rnaturalearthdata)
+library(spData)
 library(tidyr)
 
-yr <- 2021
 df <- read.csv("data/leituras.csv") %>%
-  filter(year == yr) %>%
   separate_rows(origin, sep = ";")
 
 # Spatial object
-world <- ne_countries(scale = "medium", returnclass = "sf")
+data("world")
 
-world_df <- world %>% filter(admin %in% unique(df$origin))
+yr = 2023
+df <- df %>%
+  filter(year == yr)
 
-world_points <- cbind(world_df, st_coordinates(st_centroid(world_df$geometry)))
 
-head(world_df)
+world_df <- world %>% filter(name_long %in% unique(df$origin))
 
 mapa <- ggplot() +
   geom_sf(data = world, fill = "grey", alpha = 0.5, color = "white") +
   geom_sf(data = world_df, fill = "red", alpha = 0.5, color = "white") +
-  #geom_text(data = world_points, aes(x = X, y = Y, label = name),
-   #         color = "darkblue", fontface = "bold", check_overlap = FALSE) +
-  #geom_sf(data = df_cen, color = 'red') +
   theme_void() +
   labs(title = paste("Origem de autoras(es) das leituras de", yr)) +
   theme(legend.position = "none")
@@ -33,4 +28,9 @@ mapa <- ggplot() +
 mapa
 ggsave(paste0("figs/mapa_", yr, ".png"))
 
-table(df$gender)
+
+df %>%
+  distinct(author, gender) %>%
+  group_by(gender) %>%
+  summarize(length(gender))
+
